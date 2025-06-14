4ea715da99bff5b42c5964eabe6de4d5f25b0e87
Shahar Mayster <shaharyes21@gmail.com>
2025-06-14 12:42:58 +0300

    fix: added clientId and appId to frontegg.config.js

--- src/frontegg.config.js
+++ src/frontegg.config.js
@@ -1,3 +1,5 @@
 export const contextOptions = {
   baseUrl: 'https://shaharmayster.frontegg.com',
+  clientId: 'd792a79c-a4a6-4368-a829-7610367e8a2d',
+  appId: 'd792a79c-a4a6-4368-a829-7610367e8a2d'
 };
