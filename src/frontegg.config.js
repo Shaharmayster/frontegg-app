c4f75811c67ac84334ba3b9f118bd9b29a9c2abb
Shahar Mayster <shaharyes21@gmail.com>
2025-06-14 12:49:32 +0300

    fix: corrected baseUrl in frontegg.config.js to match project domain

--- src/frontegg.config.js
+++ src/frontegg.config.js
@@ -1,5 +1,5 @@
 export const contextOptions = {
-  baseUrl: 'https://shaharmayster.frontegg.com',
+  baseUrl: 'https://app-2s55dwpeeoii.frontegg.com',
   clientId: 'd792a79c-a4a6-4368-a829-7610367e8a2d',
   appId: 'd792a79c-a4a6-4368-a829-7610367e8a2d'
 };
